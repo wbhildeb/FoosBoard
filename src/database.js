dd728a8c95bf0c60ea0a69aa6db473aee78cdad0
whildebr <Walker_Hildebrand@mcafee.com>
2019-11-22 16:15:05 -0500

    Update database

--- src/database.js
+++ src/database.js
@@ -31,78 +31,11 @@ class DocRef
    */
   static Team(team)
   {
-    if (!team) return Promise.reject('Empty param: team');
+    var teamID = typeof team === 'string' ?
+      team :
+      Database.GetTeamID(team);
 
-    if (typeof team === 'string')
-    {
-      return CollectionRef.Teams().doc(team);
-    }
-
-    var userIDs;
-    if (!team[0]) return Promise.reject('Bad param: team');
-    if (team[0].id)
-    {
-      userIDs = team.map(user => user.id);
-    }
-    else
-    {
-      userIDs = team;
-    }
-
-    userIDs.sort();
-
-    return new Promise((resolve, reject) =>
-    {
-      DocRef
-        .User(userIDs[0])
-        .get()
-        .then(
-          snapshot => 
-          {
-            if (!snapshot.exists) reject('No team with given users');
-            
-            // Get all teams that the first user is on
-            var teamPromises = [];
-            Object
-              .keys(snapshot.data().teams)
-              .forEach(teamID =>
-              {
-                teamPromises.push(DocRef.Team(teamID).get())
-              });
-              
-            Promise
-              .all(teamPromises)
-              .then(snapshots =>
-              {
-                // Go through each team and see if the members match the given users
-                for (let i = 0; i < snapshots.length; ++i)
-                {
-                  if (snapshots[i].exists)
-                  {
-                    if (utils.ArraysEqual(userIDs, Object.keys(snapshots[i].data().members)))
-                    {
-                      resolve(snapshots[i].ref);
-                    }
-                    return;
-                  }
-                }
-
-                resolve('Not team with given users');
-              });
-            },
-            reject);
-    });
-
-  }
-
-  /**
-   * Returns the team with the specified users or undefined if none exists
-   * @param {string[]|User[]} userIDs 
-   * @returns {Promise<DatabaseReference>}
-   */
-  static GetTeamRef(userIDs)
-  {
-    
+    return CollectionRef.Teams().doc(teamID);
   }
 }
 
@@ -114,83 +47,39 @@ module.exports = class Database
   {
     var userRef = DocRef.User(userID);
 
-    return userRef
-      .get()
-      .then(snapshot =>
-      {
-        if (snapshot.exists)
-        {
-          const oldname = snapshot.data().name; 
-          if (oldname != name)
-          {
-            return userRef
-              .update({name})
-              .then(() => console.log(`Update user [${userID}] name: ${oldname} -> ${name}`));
-          }
-        }
-        else
-        {
-          return userRef
-            .set({name})
-            .then(() => console.log(`Create user [id: ${userID}, name:${name}]`));
-        }
-      });
-  }
-
-  /**
-   * Returns true if the user exists in the database
-   * @param {string} userID 
-   * @returns {Promise<boolean>}
-   */
-  static UserExists(userID)
-  {
-    return DocRef
-      .User(userID)
-      .get()
-      .then(snapshot => 
-      {
-        return snapshot.exists;
-      });
-  }
-
-  /**
-   * Returns true if the team with the specified users exists
-   * @param {string[]|User[]} users 
-   * @returns {Promise<Team>}
-   */
-  static DoesTeamExist(users)
-  {
-    return this.GetTeam(users).then(val => !!val);
-  }
-
-  /**
-   * Returns the team with the specified users or undefined if none exists
-   * @param {string[]|User[]} userIDs 
-   * @returns {Promise<DatabaseReference>}
-   */
-  static GetTeam(userIDs)
-  {
     return new Promise((resolve, reject) =>
     {
-      DocRef
-        .Team(userIDs)
-        .then(ref =>
+      userRef
+        .get()
+        .then(snapshot =>
         {
-          ref
-            .get()
-            .then(snapshot =>
+          if (snapshot.exists)
+          {
+            const oldname = snapshot.data().name; 
+            if (oldname != name)
             {
-              if (snapshot.exists)
-                resolve(snapshot.data());
-              else
-                reject('Team does not exist');
-            });
-        })
-        .catch(reject);
+              userRef
+                .update({name})
+                .then(() =>
+                {
+                  console.log(`Update user [${userID}] name: ${oldname} -> ${name}`);
+                  resolve();
+                });
+            }
+          }
+          else
+          {
+            return userRef
+              .set({name})
+              .then(() =>
+              {
+                console.log(`Create user [id: ${userID}, name:${name}]`);
+                resolve();
+              });
+          }
+        });
     });
   }
-
-  
 
   /**
    * Returns the members object that would be stored in a team for the given users
@@ -216,41 +105,98 @@ module.exports = class Database
     return members;
   }
 
-//   static AddTeam(users, teamName)
-//   {
-//     if (!teamName)
-//     {
-//       // Set Default team name from team members
-//       teamName = '';
-//       users.forEach(user => {
-//         teamName += user.name + ' ';
-//       });
-//       teamName = teamName.trimEnd();
-//     }
+  static AddUserToTeam(user, teamID)
+  {
+    return new Promise((resolve, reject) =>
+    {
+      this
+        .AddUser(user.id, user.name)
+        .then(() =>
+        {
+          DocRef
+            .User(user.id)
+            .update({
+              teams:
+              {
+                [teamID]: true
+              }})
+            .then(() =>
+            {
+              console.log(`Update user [id: ${user.id}] teams: added ${teamID}`);
+              resolve();
+            })
+        });
+    });
+  }
 
-//     var members = {};
-//     var promises = [];
-//     users.forEach(user =>
-//     {
-//       // Ensure user exists
-//       promises.push(this.AddUser(user.id, user.name));
-//       members[user.id] = true;
-//     });
+  static GetTeamID(users)
+  {
+    var userIDs;
+    if (team[0].id)
+    {
+      userIDs = team.map(user => user.id);
+    }
+    else
+    {
+      userIDs = team;
+    }
 
-//     // Await addition of all users
-//     Promise
-//       .all(promises)
-//       .then(() =>
-//       {
+    userIDs.sort();
+    var teamID = '';
+    userIDs.forEach(id => teamID += id);
+  }
 
-//       });
+  static AddTeam(users, teamName)
+  {
+    if (!teamName)
+    {
+      // Set Default team name from team members
+      teamName = '';
+      users.forEach(user => {
+        teamName += user.name + ' ';
+      });
+      teamName = teamName.trimEnd();
+    }
 
+    const teamID = this.GetTeamID(users);
+    const teamRef = DocRef.Team(teamID); 
 
-//     CollectionRef
-//       .Teams()
-//       .set({
-//         name: teamName,
-//         users: userIDs
-//       });
-//   }
+    return new Promise((resolve, reject) =>
+    {
+      teamRef
+        .get()
+        .then(snapshot =>
+        {
+          if (snapshot.exists && snapshot.data().name != teamName)
+          {
+            teamRef
+              .update({ name: teamName })
+              .then(() =>
+              {
+                console.log(`Update user [${userID}] name: ${oldname} -> ${name}`);
+                resolve();
+              });
+          }
+          else
+          {
+            const promises = [];
+            promises.push(
+              teamRef.set({
+                name: teamName
+              })
+            );
+            
+            users.forEach(user =>
+            {
+              promises.push(this.AddUserToTeam(user, teamID));
+            });
+
+            Promise.all(promises).then(() =>
+            {
+              resolve();
+            });
+          }
+        });
+    });
+  }
 };
--- src/testdb.js
+++ src/testdb.js
@@ -1,12 +1,12 @@
 const env = require('./environment');
 const db = require('./database');
 
-db.AddUser('U0000', 'Patient 0')
-  .then(() => db.AddUser('U0001', 'Patient One'))
-  .then(() => db.AddUser('U0000', 'Patient Zero'))
-  .then(() => db.AddUser('U0000', 'Patient Zero'));
+// db.AddUser('U0000', 'Patient 0')
+//   .then(() => db.AddUser('U0001', 'Patient One'))
+//   .then(() => db.AddUser('U0000', 'Patient Zero'))
+//   .then(() => db.AddUser('U0000', 'Patient Zero'));
 
-db.GetTeam([{ id: 'U0000'}, { id: 'U0001'}]).then(console.log);
-// db.AddTeam(['U0000', 'U0001'], 'The Patients');
-
-// console.log(env.DEBUG);
+db.AddTeam([ {id: 'U0000', name: 'Patient Zero'}, {id: 'U0001', name: 'Patient One'} ], 'The Patience')
+  .then(
+    () => db.AddTeam([ {id: 'U0000', name: 'Patient Zero'}, {id: 'U0001', name: 'Patient One'} ], 'The Patients')
+);
