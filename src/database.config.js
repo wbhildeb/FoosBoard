04397384051dbb38987346dd10d229c29670dbfd
Walker Hildebrand <walker99113@gmail.com>
2019-11-25 19:49:58 -0500

    Expose database config
    Expose database config

---
+++ src/database.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyB506K-0h2bz0HWoRX-BpAYD0JcV7MzCOw",
+  authDomain: "wbh-foosboard.firebaseapp.com",
+  databaseURL: "https://wbh-foosboard.firebaseio.com",
+  projectId: "wbh-foosboard",
+  storageBucket: "wbh-foosboard.appspot.com",
+  messagingSenderId: "48722104241",
+  appId: "1:48722104241:web:d107740f1dc84ed9f43790",
+  measurementId: "G-39Y3VXKR1N"
+};
--- src/environment.js
+++ src/environment.js
@@ -1,2 +1,2 @@
 module.exports.DEBUG = process.env.DEBUG === 'true';
-module.exports.FIREBASE_INFO = require('./db-secret');
+module.exports.FIREBASE_INFO = require('./database.config');
